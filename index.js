eab419d27072b5ddd0032f52d188faf35e7077b2
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-03-28 11:42:39 -0400

    modify elements lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory" ;
+
+newHeader.textContent="Brian is the champion";
